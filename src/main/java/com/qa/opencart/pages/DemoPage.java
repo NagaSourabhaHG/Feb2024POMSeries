0e42b9e6f01eddb947cede3dfa03e135431edede
NagaSourabha <sourihgs@gmail.com>
2024-07-06 18:16:57 +0530

    Committing changes as per PR comments

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -4,8 +4,11 @@ public class DemoPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//printing the statement 
 		
 		System.out.println("Test");
+		System.out.println("Add to cart");
+		
 
 	}
 
