69bce01cc70b0ac9d541d4adc92c33cbb3d6a7a4
NagaSourabhaHG <52439273+NagaSourabhaHG@users.noreply.github.com>
2024-07-07 17:10:58 +0530

    Update Login.java

--- src/main/java/com/qa/opencart/pages/Login.java
+++ src/main/java/com/qa/opencart/pages/Login.java
@@ -2,6 +2,15 @@ package com.qa.opencart.pages;
 
 public class Login {
 
+	public void Login()
+	{
+	System.out.println("Test");
+        System.out.println("abc");
+
+		
+	}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s ="Test";
