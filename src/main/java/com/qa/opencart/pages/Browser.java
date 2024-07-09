86ac5d57bc0aa520762033b4496333c86fa16ff4
NagaSourabha <sourihgs@gmail.com>
2024-07-09 20:11:58 +0530

    deleting login and browser files

---
+++ src/main/java/com/qa/opencart/pages/Browser.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("abcd");
+
+	}
+
+}
