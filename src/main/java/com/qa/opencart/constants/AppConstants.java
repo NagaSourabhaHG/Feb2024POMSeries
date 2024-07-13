3f8888cb8e8a3e8f9206d590c87c32cb5419fc49
NagaSourabha <sourihgs@gmail.com>
2024-07-13 15:35:22 +0530

    Adding changes of AppConstants

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class AppConstants {
 
-	public static final String LOGIN_PAGE_TITLE = "Account Login11";
-	public static final String ACCOUNTS_PAGE_TITLE = "My Account11";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
+	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
 	public static final String LOGIN_PAGE_FRACTION_URL = "route=account/login";
 	public static final String CONFIG_FILE_PATH = "./src/test/resources/config/config.properties";
 	public static final String CONFIG_QA_FILE_PATH = "./src/test/resources/config/qa.properties";
